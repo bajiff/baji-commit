2c1160d7edfbb215640324e6dca4e706a77263b0
BajiLinux <mr.baji.smart@gmail.com>
2026-03-10 01:31:12 +0700

    Done

--- index.test.js
+++
@@ -1,8 +0,0 @@
-import assert from "node:assert";
-import test from "node:test";
-import sum from "./index.js";
-
-test("Berjalan", () => {
-  assert.strictEqual(sum(10,30),40);
-  assert.strictEqual(sum(null,null),0);
-});
